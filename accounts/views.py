9cc568f670acdb3526b2bc29f058ebbe16503643
Alexander Kurochkin <110339661+admiralbanan@users.noreply.github.com>
2025-03-26 00:47:52 +0300

    Add files via upload
    ADD API

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,15 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
+from rest_framework import viewsets
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')  # Перенаправление на страницу входа после регистрации
+    success_url = reverse_lazy('login')  
     template_name = 'accounts/signup.html'
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,19 @@
 # myproject/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from accounts.views import CustomUserViewSet
+from myapp.views import PersonViewSet, CarAccidentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', CustomUserViewSet)
+router.register(r'persons', PersonViewSet)
+router.register(r'accidents', CarAccidentViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),  # Подключение путей accounts
-    path('accounts/', include('django.contrib.auth.urls')),  # URL-адреса для авторизации
-    path('', include('myapp.urls')),  # Подключаем маршруты из myapp
+    path('api/', include(router.urls)),  # ← вот это добавляем
 ]
